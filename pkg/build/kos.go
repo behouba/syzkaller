c58a03bdead178a06a6f8e5e0ef7dda980407f9d
Fahima Mokhtari <df_mokhtari@esi.dz>
2022-06-09 07:18:57 -0400

    pkg/build: add kos build
    * pkg/build: add kos builder
    
    * vm/qemu/qemu.go: add kos config
    
    * pkg/build: add kos builder
    
    * pkg/build: update kos builder

---
+++ pkg/build/kos.go
@@ -0,0 +1,70 @@
+// Copyright 2018 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package build
+
+import (
+	"fmt"
+	"path/filepath"
+	"time"
+
+	"github.com/google/syzkaller/pkg/osutil"
+	"github.com/google/syzkaller/sys/targets"
+)
+
+type kos struct{}
+
+// Path to the root directory.
+const SYSROOT = "/opt/KasperskyOS-Community-Edition-1.0.0.69/sysroot-arm-kos/"
+
+// Path to the toolchain.
+const TOOLCHAIN = "/opt/KasperskyOS-Community-Edition-1.0.0.69/toolchain/"
+
+// Path to the linker script required for the solution build.
+const LINKERSCRIPT = "/opt/KasperskyOS-Community-Edition-1.0.0.69/libexec/arm-kos/"
+
+// Path to the precompiled KOS kernel not containing romfs.
+const IMGSRC = "/opt/KasperskyOS-Community-Edition-1.0.0.69/libexec/arm-kos/"
+
+func (ka kos) build(params Params) (ImageDetails, error) {
+	sysTarget := targets.Get(targets.KOS, params.TargetArch)
+	if sysTarget == nil {
+		return ImageDetails{}, fmt.Errorf("unsupported Kaspersky arch %v", params.TargetArch)
+	}
+	arch := sysTarget.KernelHeaderArch
+	kernelKOS := filepath.Join(params.KernelDir, "out", arch, "kos-image")
+	if _, err := runSandboxed(time.Hour, params.KernelDir,
+		"makeimg --target="+arch, "--sys-root="+SYSROOT,
+		"--with-toolchain="+TOOLCHAIN,
+		"--ldscript="+LINKERSCRIPT,
+		"--img-src="+IMGSRC,
+		"--img-dst="+kernelKOS,
+	); err != nil {
+		return ImageDetails{}, err
+	}
+	if _, err := runSandboxed(time.Hour*2, params.KernelDir, "scripts/fx", "clean-build"); err != nil {
+		return ImageDetails{}, err
+	}
+
+	// Add ssh keys to the zbi image so syzkaller can access the KOS vm.
+	_, sshKeyPub, err := genSSHKeys(params.OutputDir)
+	if err != nil {
+		return ImageDetails{}, err
+	}
+
+	sshKOS := filepath.Join(params.OutputDir, "initrd")
+
+	authorizedKeys := fmt.Sprintf("data/ssh/authorized_keys=%s", sshKeyPub)
+
+	if _, err := osutil.RunCmd(time.Minute, params.KernelDir, "out/"+arch+"/host_x64/kos",
+		"-o", sshKOS, kernelKOS, "--entry", authorizedKeys); err != nil {
+		return ImageDetails{}, err
+	}
+	return ImageDetails{}, nil
+}
+
+func (ka kos) clean(kernelDir, targetArch string) error {
+	// We always do clean build because incremental build is frequently broken.
+	// So no need to clean separately.
+	return nil
+}
